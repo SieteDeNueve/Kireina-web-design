0fc829777c503c538c27291d5e81cfb4e1e10582
SieteDeNueve <daliborkajekic@gmail.com>
2018-10-08 20:16:26 +0200

    Minified our HTML files

--- components/coffee/tagline.coffee
+++ components/coffee/tagline.coffee
@@ -1,5 +1,5 @@
 $ = require 'jquery'
 
 do fill = (item = 'Dizajn napravljen specijalno za vas.') ->
-  $('.tagline').append "#{item}"
+  $('.tagline').append "{item}"
 fill
--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -3,7 +3,7 @@ var $, fill;
 $ = require('jquery');
 
 (fill = function(item) {
-  return $('.tagline').append(`${item}`);
+  return $('.tagline').append("{item}");
 })('Dizajn napravljen specijalno za vas.');
 
 fill;
