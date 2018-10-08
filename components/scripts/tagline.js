54b84aa153942eb7aab701cfeed06458e3dfe3da
SieteDeNueve <daliborkajekic@gmail.com>
2018-10-08 12:43:27 +0200

    Added Browserify

--- components/scripts/tagline.js
+++ components/scripts/tagline.js
@@ -3,7 +3,7 @@ var $, fill;
 $ = require('jquery');
 
 (fill = function(item) {
-  return $('.tagline').append('${item}');
+  return $('.tagline').append(`${item}`);
 })('Dizajn po vašoj meri');
 
 fill;
